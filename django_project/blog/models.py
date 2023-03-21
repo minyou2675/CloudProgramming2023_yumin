c90fd42bf6f630403a19b5a807615d2e47a4d76b
minyou2675 <kymyy2675@gmail.com>
2023-03-21 15:22:22 +0900

    updated at 추가

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField() #글자수 제한 X
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True) # 컬럼에 추가될 때 생김
+    updated_at = models.DateTimeField(auto_now=True) # 자동추가
     def __str__(self): #object 이름을 table로 반환하도록 str 함수를 오버라이딩
         return f'{[self.pk]} {self.title}'
