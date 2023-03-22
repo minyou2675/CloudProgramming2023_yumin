3c58fa8d32533b4100d073307d8f47356fba6b03
minyou2675 <kymyy2675@gmail.com>
2023-03-22 23:51:30 +0900

    게시글 목록 추가

---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from .models import Post
+
+
+# Create your views here.
+def index(request):
+    # pk는 자동생성
+    posts = Post.objects.all().order_by('-pk')
+
+    return render(request, 'blog/index.html', {"posts": posts})
+
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/single_post_page.html', {'post': post})
