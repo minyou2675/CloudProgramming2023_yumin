095509624d14d47b4a41657b071559374315d669
minyou2675 <kymyy2675@gmail.com>
2023-03-28 17:04:14 +0900

    static files introduced, images attachment, file attachment

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,11 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField() #글자수 제한 X
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/',blank=True) #blank -> 필수항목 아님, upload to 경로에 이미지 저장
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d',blank=True)
     created_at = models.DateTimeField(auto_now_add=True) # 컬럼에 추가될 때 생김
     updated_at = models.DateTimeField(auto_now=True) # 자동추가
     def __str__(self): #object 이름을 table로 반환하도록 str 함수를 오버라이딩
         return f'{[self.pk]} {self.title}'
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
